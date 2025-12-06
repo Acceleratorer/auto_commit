42e5199d869a102e56b029394f17f423c830d745
Acceleratorer <noobtan77@gmail.com>
2025-12-07 00:25:24 +0700

    done to add something new, but i forgot what new

--- autocommit.py
+++ autocommit.py
@@ -2,4 +2,14 @@ import os
 import datetime 
 import subprocess
 
-REPO PATH = r'/path/to/your/repo'
+REPO_PATH = r"E:/Code/Github/autocommit"
+
+os.chdir(REPO_PATH)
+
+with open ("log.txt", "a") as log_file:
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    log_file.write(f"Autocommit at {timestamp}\n")
+    
+subprocess.run(["git", "add", "."])
+subprocess.run(["git", "commit", "-m", f"Autocommit {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"])
+subprocess.run(["git", "push"])
