288bec09911b71f2e438c09e9dbe0bd2669b6cb9
Acceleratorer <noobtan77@gmail.com>
2025-12-16 21:58:07 +0700

    small change

--- run_autocommit.bat
+++ run_autocommit.bat
@@ -1,2 +1,2 @@
 @echo off
-"C:\Users\goddt\AppData\Local\Programs\Python\Python314\python.exe" E:\Code\Github\autocommit\autocommit_v2.py
+"C:\Users\goddt\AppData\Local\Programs\Python\Python314\python.exe" E:\Code\Github\autocommit\autocommit.py
