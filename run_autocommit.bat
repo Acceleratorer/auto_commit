7b14bcbb82d2380df3ba23d2ed6f12a9c0344b34
Acceleratorer <noobtan77@gmail.com>
2025-12-18 10:00:02 +0700

    General improvement

--- run_autocommit.bat
+++ run_autocommit.bat
@@ -1,2 +1,10 @@
 @echo off
-"C:\Users\goddt\AppData\Local\Programs\Python\Python314\python.exe" E:\Code\Github\autocommit\autocommit.py
+cd /d "E:\Code\Github\autocommit"
+
+set LOG=log_%date:~10,4%%date:~4,2%%date:~7,2%.txt
+
+echo ==== Run at %date% %time% ==== >> %LOG%
+
+"C:\Users\goddt\AppData\Local\Programs\Python\Python314\python.exe" autocommit.py >> %LOG% 2>&1
+
+echo ---- Script Finished ---- >> %LOG%
